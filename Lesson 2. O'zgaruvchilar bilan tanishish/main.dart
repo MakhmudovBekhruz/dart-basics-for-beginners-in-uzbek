cdb2f5c9360dcc175f4e0494bea534170c0b8caf
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-21 22:21:36 +0500

    Lesson 2

---
+++ Lesson 2. O'zgaruvchilar bilan tanishish/main.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  // int level = 9; //value 9
+  // print(level);
+  // level = 15;
+  // print(level);
+
+  int a = 12;
+  int b = 34;
+
+  int c = a + b;
+
+  print(c);
+}
