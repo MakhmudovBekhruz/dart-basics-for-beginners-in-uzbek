3ca96a67242effe277f781e8acad883297280fa1
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-21 23:37:24 +0500

    Lesson 5

---
+++ Lesson 5, boolean o'zgaruvchilar/main.dart
@@ -0,0 +1,28 @@
+main(List<String> args) {
+  // bool a = true;
+  // print(a);
+  // bool isTurnOn = false;
+
+  // int a = 15;
+  // int b = 20;
+  // bool c = a != b;
+
+  // bool f = false; // !(true) = false;  !(false) = true
+  // bool g = !(!f);
+
+  // print(g);
+
+  // print(c);
+
+  // int a = 14;
+  // int b = 18;
+
+  // bool c = a >= b;
+  // print(c);
+
+  int a = 14;
+  int b = 18;
+
+  bool c = a <= b;
+  print(c);
+}
