1fa53ee8237c8f3366d2bd9edd3900fb9168feb4
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-21 23:06:19 +0500

    Lesson 4

---
+++ Lesson 4, String turidagi o'zgaruvchilar/main.dart
@@ -0,0 +1,17 @@
+main(List<String> args) {
+  // String a = "A'lochi";
+  // String b = 'Salom';
+  // print(a);
+  // print(b);
+
+  // print(a + " " + b);
+
+  String a = "2";
+  String b = '3';
+  int c = 2;
+  // print(a);
+  // print(b);
+
+  // print(a + c.toString());
+  print(double.parse("8.6") + c); //8.6  + 2 = 10.6
+}
