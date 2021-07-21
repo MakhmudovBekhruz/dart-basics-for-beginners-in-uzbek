ed015b15712df7356a04c0cee273a6c9d63e9843
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-21 22:43:41 +0500

    Lesson 3

---
+++ Lesson 3. O'zgaruvchilar bilan ishlash/main.dart
@@ -0,0 +1,17 @@
+main(List<String> args) {
+  // int a = 16;
+  // int b = 5;
+
+  // double c = a / b; //8.9,   10.56
+
+  // print(c);
+
+  int a = 17;
+  int b = 5;
+
+  double d = a / b;
+  print(d);
+  int f = d.toInt();
+
+  print(f);
+}
