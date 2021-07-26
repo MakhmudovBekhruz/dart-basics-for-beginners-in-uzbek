e7b0c29503ded8ebe32ab01065bed80ba98ad5ff
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-27 04:18:46 +0500

    Lesson 9

---
+++ Lesson 9, shart operatori ternar/main.dart
@@ -0,0 +1,20 @@
+main(List<String> args) {
+  // shartQiymati ? rostBolgandagiQiymat : yolgon bolgandagiQiymat
+
+  // bool a = 8 < 17;
+  // String b = "";
+  // if (a) {
+  //   b = "A rost";
+  // } else {
+  //   b = "A yolgon";
+  // }
+
+  // print(b);
+  // bool a = 8 < 17;
+  // String b = a ? "A rost" : "A yolgon";
+  // print(b);
+
+  bool a = 18 < 17;
+  String b = a ? "A rost" : "A yolgon";
+  print(b);
+}
