e1353f7402d0b10a87c3f30dbcff8d735c4cefc6
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-27 03:17:09 +0500

    Lesson 6

---
+++ Lesson 6. List/main.dart
@@ -0,0 +1,24 @@
+main(List<String> args) {
+  List a = [23, 56, 6, 15]; //0, 1, 2, 3
+  List b = [4, 6];
+
+  print(a);
+
+  // print(a[0]);
+  // print(a[17]);
+
+  // print(a.length);
+
+  // a.add(2);
+  // print(a.length);
+
+  // a.addAll(b);
+
+  // a.clear();
+
+  print(a.contains(57));
+
+  // print(a.isEmpty);
+
+  print(a);
+}
