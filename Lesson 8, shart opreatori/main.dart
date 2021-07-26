ff1089724f2e45656c418376634fa9649f398d05
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-27 04:02:31 +0500

    Lesson 8

---
+++ Lesson 8, shart opreatori/main.dart
@@ -0,0 +1,31 @@
+main(List<String> args) {
+  // bool a = 8 > 3;
+  // a = false;
+  // print(a);
+  // a = false;
+  // if (a) {
+  //   print(" 8 3 dan katta");
+  // } else {
+  //   print("8 3 dan katta emas");
+  // }
+
+  // bool a = true;
+  // bool b = false;
+
+  // bool c = a && b; // true * false = 1 * 0 = 0
+  // //1 * 1 = 1; 1 * 0 = 0; 0 * 1 = 0; 0 * 0 = 0
+
+  // if (c) {
+  //   print("A va B hodisa rost");
+  // }
+
+  bool a = true;
+  bool b = false;
+
+  bool c = a || b; // true + false = 1 + 0 = 1
+  //1 + 1 = 1; 1 + 0 = 1; 0 + 1 = 1; 0 + 0 = 0
+
+  if (c) {
+    print("A yoki B hodisa rost");
+  }
+}
