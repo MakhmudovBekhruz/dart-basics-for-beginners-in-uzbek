618627ef7f07e47ec85e07314c4997ffb4e1ea48
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-07-27 04:28:42 +0500

    Lesson 10

---
+++ Lesson 10, switch operatori/main.dart
@@ -0,0 +1,32 @@
+main(List<String> args) {
+  // int a = 1;
+  // switch (a) {
+  //   case 23:
+  //     print("A 23 ga teng");
+  //     break;
+  //   case 12:
+  //     print("A 12 ga teng");
+  //     break;
+  //   case 67:
+  //     print("A 67 ga teng");
+  //     break;
+  //   case 89:
+  //     print("A 89 ga teng");
+  //     break;
+  // }
+
+  String a = "START";
+
+  switch (a) {
+    case "START":
+      print("Started");
+      break;
+    case "END":
+      print("completed");
+      break;
+
+    case "ON_GOING":
+      print("on going");
+      break;
+  }
+}
