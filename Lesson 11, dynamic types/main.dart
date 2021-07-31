57f9668e399c9dd6817d97a4b9211cd8b0a39275
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-01 02:54:15 +0500

    Lesson 11

---
+++ Lesson 11, dynamic types/main.dart
@@ -0,0 +1,17 @@
+main(List<String> args) {
+  // String a = "salom";
+  // int b = 23;
+
+  // print(a is int);
+  // print(b is int);
+
+  // dynamic a = "salom";
+  // a = 13;
+
+  // print(a is int);
+
+  // var a = "salom";
+  // // String a = "salom";
+  // var b = 13;
+  // print(a is String);
+}
