af228acc2e59e8e7a12d1b9e6d965e4ae305bc0c
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-01 03:38:36 +0500

    Lesson 13

---
+++ Lesson 13, string qo'shimcha funksiyalar/main.dart
@@ -0,0 +1,26 @@
+main(List<String> args) {
+  // String a = "Hello World.";
+  // print(a.isEmpty);
+  // String a = "";
+  // print(a.isNotEmpty);
+  // String a = "Hello World";
+  // print(a.length);
+  // String a = "Hello World";
+  // print(a.contains("lo"));
+  // String a = "Hello World";
+  // print(a.endsWith("ld"));
+  // String a = "Hello World";
+  // print(a.startsWith("He"));
+  // String a = "Hello World";
+  // print(a.indexOf("W"));
+  // String a = "Hello World, I love programming";
+  // List<String> list = a.split(",");
+
+  // print(list);
+
+  // String a = "           Hello World             ";
+  // print(a.trim());
+
+  String a = "Hello World";
+  print(a.replaceAll("W", "F"));
+}
