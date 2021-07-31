bb8a551deb36465c817b610715a0bbcd95fe66c9
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-01 03:14:34 +0500

    Lesson 12

---
+++ Lesson 12, null safety/main.dart
@@ -0,0 +1,17 @@
+main(List<String> args) {
+  // String a = "salom";
+  // String? b = "hello";
+  // b = null;
+  // print(b);
+  // final String? a = null;
+  // print(a);
+
+  String b = "hello";
+  const String a = "salom";
+  // final String? a = null;
+
+  ///compile time
+  ///run time
+  ///
+  print(a);
+}
