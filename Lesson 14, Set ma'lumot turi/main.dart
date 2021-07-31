ce5c9c20724b5a3026ea86542bb5b645fd4cf2d0
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-01 03:56:22 +0500

    Lesson 14

---
+++ Lesson 14, Set ma'lumot turi/main.dart
@@ -0,0 +1,37 @@
+main(List<String> args) {
+  // List<String> a = ["a", "a", "c", "c"];
+  // print(a);
+  // Set<String> a = {"a", "a", "a", "a", "a", "a", "a", "a", "b", "c"};
+
+  // a.add("b");
+  // print(a);
+
+  // Set<String> a = {"a", "b", "c"};
+
+  // print(a.length);
+  // Set<String> a = {"a", "b", "c"};
+
+  // print(a.first);
+  // Set<String> a = {"a", "b", "c"};
+
+  // print(a.last);
+  // Set<String> a = {"a", "b", "c"};
+  // a.add("d");
+  // print(a);
+  // Set<String> a = {"a", "b", "c"};
+  // a.clear();
+  // print(a);
+  // Set<String> a = {"a", "b", "c"};
+
+  // print(a.contains("e"));
+
+  // Set<String> a = {"a", "b", "c"};
+
+  // print(a.containsAll(["a", "b"]));
+  // Set<String> a = {"a", "b", "c"};
+
+  // print(a.elementAt(0));
+  Set<String> a = {"a", "b", "c"};
+  a.remove("c");
+  print(a);
+}
