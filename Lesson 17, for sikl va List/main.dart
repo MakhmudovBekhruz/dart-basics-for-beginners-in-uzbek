f3ec3ab3a1ac90b0879d5da8a95403f1d4b8205d
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-02 01:41:33 +0500

    Lesson 17

---
+++ Lesson 17, for sikl va List/main.dart
@@ -0,0 +1,24 @@
+main(List<String> args) {
+  // List<String> sozlar = ["olma", "anor", "banan", "nok"];
+
+  // // print(sozlar[0]);
+  // int uzunligi = sozlar.length; //4
+
+  // for (int i = 0; i < uzunligi; i++) {
+  //   print(sozlar[i]);
+  // }
+
+  // for (int i = 1; i < 10; i++) {
+  //   if (i % 2 == 0) {
+  //     print("i juft son : " + i.toString());
+  //   } else {
+  //     print("i toq son : " + i.toString());
+  //   }
+  // }
+
+  for (int i = 50; i > 30; i--) {
+    if (i % 4 == 1) {
+      print(i);
+    }
+  }
+}
