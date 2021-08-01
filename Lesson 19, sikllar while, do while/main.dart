21c80012358b6413bcc0a5cbcf0ad7ffdfe90d9d
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-02 02:10:34 +0500

    Lesson 19

---
+++ Lesson 19, sikllar while, do while/main.dart
@@ -0,0 +1,20 @@
+main(List<String> args) {
+  // int i = 0;
+  // while (i < 3) {
+  //   print("hello, i : " + i.toString());
+  //   i = i + 1;
+  // }
+
+  // int i = 10;
+  // while (i >= 0) {
+  //   print("hello, i : " + i.toString());
+  //   i = i - 1;
+  // }
+
+  int i = 0;
+  do {
+    //sikl kodi
+    print(i);
+    i++;
+  } while (i < 10);
+}
