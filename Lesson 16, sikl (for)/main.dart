0a6e635264d7901df588c88ec09a6097f96df118
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-02 01:29:57 +0500

    Lesson 16

---
+++ Lesson 16, sikl (for)/main.dart
@@ -0,0 +1,24 @@
+main(List<String> args) {
+  // int i = 1;
+  // i -= 6;
+  // i--;//i = i -1
+  // i = i + 6;
+  // print(i);
+
+  // for (int i = 0; i <= 10; i += 4) {
+  //   print(i);
+  // }
+  // //true 0
+  // //true 4
+  // //true 8
+  // //false 12
+
+  for (int i = 20; i >= 0; i -= 5) {
+    print(i);
+  }
+  //true 20
+  //true 15
+
+  //....true 0
+  //- 5 false
+}
