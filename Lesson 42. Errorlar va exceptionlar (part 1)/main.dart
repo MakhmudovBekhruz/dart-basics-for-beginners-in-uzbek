bbe1088091009252eac0c274989975d92d8cff2f
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-18 01:04:59 +0500

    Lesson 42

---
+++ Lesson 42. Errorlar va exceptionlar (part 1)/main.dart
@@ -0,0 +1,10 @@
+main(List<String> args) {
+  //error = xato
+  //exception = istisno, mustasno
+
+  // List<int> a = [4, 8];
+  // print(a[9]);
+
+  String b = "hello";
+  print(b.substring(-40));
+}
