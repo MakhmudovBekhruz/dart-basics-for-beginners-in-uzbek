09bee7c34178220156109f566320565def821d5b
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-08-30 02:09:22 +0500

    Lesson 64

---
+++ Lesson 64. dartda cli application yaratish/dart_hello_world/bin/dart_hello_world.dart
@@ -0,0 +1,5 @@
+import 'package:dart_hello_world/dart_hello_world.dart' as dart_hello_world;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_hello_world.calculate()}!');
+}
---
+++ Lesson 64. dartda cli application yaratish/dart_hello_world/test/dart_hello_world_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_hello_world/dart_hello_world.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
